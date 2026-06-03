1a17959bd3d1d50e40e7e621f1ef97a59fab33aa
quyprox <quynguyenprox@gmail.com>
2026-06-04 03:00:08 +0700

    api get details of board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -18,6 +18,19 @@ const createNew = async (req, res, next) => {
   } catch (error) { next(error) }
 }
 
+const getDetails = async (req, res, next) => {
+  try {
+    const boardId = req.params.id
+
+    // Điều hướng dữ liệu sang tầng Service
+    const board = await boardService.getDetails(boardId)
+
+    // Có kết quả thì trả về phía Client
+    res.status(StatusCodes.OK).json(board)
+  } catch (error) { next(error) }
+}
+
 export const boardController = {
-  createNew
+  createNew,
+  getDetails
 }
