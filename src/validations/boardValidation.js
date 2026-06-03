28bf92335bd1a80928ee89ec348b5f98dcffd700
quyprox <quynguyenprox@gmail.com>
2026-06-03 21:49:22 +0700

    error handling

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   /**
@@ -25,9 +26,10 @@ const createNew = async (req, res, next) => {
     // Validate dữ liệu hợp lệ thì cho request đi tiếp sang Controller
     next()
   } catch (error) {
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message
-    })
+    // const errorMessage = new Error(error).message
+    // const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    // next(customError)
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
   }
 }
 
