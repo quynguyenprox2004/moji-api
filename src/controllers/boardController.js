d60880f6c20a3c050009dbf95753c0c7e1e2a76c
quyprox <quynguyenprox@gmail.com>
2026-06-05 03:10:53 +0700

    dnd column

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -30,7 +30,20 @@ const getDetails = async (req, res, next) => {
   } catch (error) { next(error) }
 }
 
+const update = async (req, res, next) => {
+  try {
+    const boardId = req.params.id
+
+    // Điều hướng dữ liệu sang tầng Service
+    const updatedBoard = await boardService.update(boardId, req.body)
+
+    // Có kết quả thì trả về phía Client
+    res.status(StatusCodes.OK).json(updatedBoard)
+  } catch (error) { next(error) }
+}
+
 export const boardController = {
   createNew,
-  getDetails
+  getDetails,
+  update
 }
