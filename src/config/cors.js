b10b07ccc0fdf37c80094430a4b08dbbecaf0bfe
quyprox <quynguyenprox@gmail.com>
2026-06-05 17:15:29 +0700

    Update CORS

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,11 +6,13 @@ import ApiError from '~/utils/ApiError'
 // Cấu hình CORS Option trong dự án thực tế
 export const corsOptions = {
   origin: function (origin, callback) {
-    // Cho phép việc gọi API bằng POSTMAN trên môi trường dev,
-    // Thông thường khi sử dụng postman thì cái origin sẽ có giá trị là undefined
-    if (!origin && env.BUILD_MODE === 'dev') {
+    // Nếu môi trường là local dev thì cho qua luôn
+    if (env.BUILD_MODE === 'dev') {
       return callback(null, true)
     }
+
+    // Ngược lại thì hiện tại code chúng ta đang làm còn 1 trường hợp là:
+    // env.BUILD_MODE === 'production'
 
     // Kiểm tra xem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
