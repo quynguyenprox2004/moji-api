fcfb45f9ee3b432c1d293a4073385c5a2d1c4916
quyprox <quynguyenprox@gmail.com>
2026-06-05 18:17:36 +0700

    Update Whitelist domains again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // Không cần localhost nữa vì ở file config/cors đã luôn luôn cho phép môi trường dev (env.BUILD_MODE === 'dev')
-  'https://moji-web-eta.vercel.app/'
+  'https://moji-web-eta.vercel.app'
 ]
 
 export const BOARD_TYPES = {
